d39beb7b6d42ed98ebb9beaa3ca0fb2f21f697c9
Darien Payton <darien.payton71@gmail.com>
2019-08-06 21:49:07 -0500

    updated projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,11 +2,41 @@ import React from 'react';
 import Background from '../components/Background';
 import NavBar from '../components/NavBar';
 
+import '../styles/projects.scss';
+
 function Projects() {
   return (
     <Background>
       <NavBar />
-      <h3>Coming Soon</h3>
+      <div className="header">
+        <h1 className="page-header">Projects</h1>
+      </div>
+      <hr />
+      <div className="project-container">
+        <div className="project">
+          <h3>Reading Recommender (Node.js)</h3>
+          <div className="desc">
+            <p>Web application for book suggestions</p>
+            <div className="refs">
+              <a href="https://github.com/reading-recommender/reading-recommender-BE">
+                Code
+              </a>
+              <a href="https://reading-recommender.netlify.com/">Site</a>
+            </div>
+          </div>
+        </div>
+        <div className="project">
+          <h3>Portfolio Site</h3>
+          <div className="desc">
+            <p>Personal website built with React.js</p>
+            <div className="refs">
+              <a href="https://github.com/darienpayton/darienpayton-website">
+                Code
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </Background>
   );
 }
