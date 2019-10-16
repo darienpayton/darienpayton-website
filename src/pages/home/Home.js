8cd6bd1f33e2d5487b3937db697c850c13af2746
Darien Payton <darien.payton71@gmail.com>
2019-10-16 12:34:38 -0500

    updated homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import Background from '../../components/Background';
 import NavBar from '../../components/NavBar';
 import { FaGithub, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 import Subscribe from '../../components/Subscribe';
+import headshot from '../../img/DPheadshot.jpeg';
 
 import './home.scss';
 
@@ -11,17 +12,22 @@ function Home() {
     <Background>
       <NavBar />
       <div className="intro">
+        <div className="img-container">
+          <img src={headshot} alt="Darien Payton headshot" />
+        </div>
         <div className="top-content">
-          <p className="line1">Hello, my name is</p>
+          <p className="line1">Hello, I'm</p>
           <h1 className="line2">Darien Payton.</h1>
-          <p className="line3">I'm building a better future.</p>
+          <p className="line3">I'm a maker, writer, and musician</p>
           <p className="line4">
-            {' '}
-            I'm curious about improving the quality of life. Learn more about me{' '}
-            <a href="/about">here!</a>
+            I make things, write mostly short-form and play piano. I was born
+            and raised in Chicago. I spent three years living in Atlanta, before
+            moving back to Chicago. A year ago, I was running a digital
+            marketing shop, today I am a software engineer. A lot has changed,
+            and it's been a fun ride!
           </p>
         </div>
-
+        <Subscribe />
         <ul className="social">
           <li>
             <a href="https://twitter.com/darienpayton">
@@ -29,7 +35,7 @@ function Home() {
             </a>
           </li>
           <li>
-            <a href="https://github.com/dpayton23">
+            <a href="https://github.com/darienpayton">
               <FaGithub className="icon" />
             </a>
           </li>
@@ -39,7 +45,6 @@ function Home() {
             </a>
           </li>
         </ul>
-        <Subscribe />
       </div>
     </Background>
   );
