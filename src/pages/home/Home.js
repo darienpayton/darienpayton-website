a761d567e652ed6756aa82bb6b50193162717eb1
Darien Payton <darien.payton71@gmail.com>
2019-10-16 13:37:24 -0500

    fixed typo

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -23,8 +23,8 @@ function Home() {
             I make things, write mostly short-form and play piano. I was born
             and raised in Chicago. I spent three years living in Atlanta, before
             moving back to Chicago. A year ago, I was running a digital
-            marketing shop, today I am a software engineer. A lot has changed,
-            and it's been a fun ride! You can learn more about me{' '}
+            marketing consultancy, today I am a software engineer. A lot has
+            changed, and it's been a fun ride! You can learn more about me{' '}
             <a href="/about">here</a>.
           </p>
         </div>
