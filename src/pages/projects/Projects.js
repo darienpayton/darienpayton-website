62a198a6351d7262ae8bc8a06cd10608b9e1d540
Darien Payton <darien.payton71@gmail.com>
2019-10-21 16:21:10 -0500

    updated styling

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -8,32 +8,27 @@ function Projects() {
   return (
     <Background>
       <NavBar />
-      <div className="header">
-        <h1 className="page-header">Projects</h1>
+      <div className="section-header">
+        <h2 className="section-title">Projects</h2>
       </div>
-      <hr />
-      <div className="project-container">
-        <div className="project">
-          <h3>Reading Recommender (Node.js)</h3>
+      <div className="featured-container">
+        <div className="featured">
           <div className="desc">
+            <a
+              className="title"
+              href="https://reading-recommender.netlify.com/"
+            >
+              Reading Recommender
+            </a>
             <p>Web application for book suggestions</p>
-            <div className="refs">
-              <a href="https://github.com/reading-recommender/reading-recommender-BE">
-                Code
-              </a>
-              <a href="https://reading-recommender.netlify.com/">Site</a>
-            </div>
           </div>
         </div>
-        <div className="project">
-          <h3>Portfolio Site</h3>
+        <div className="featured">
           <div className="desc">
+            <a className="title" href="https://darienpayton.com">
+              Portfolio Site
+            </a>
             <p>Personal website built with React.js</p>
-            <div className="refs">
-              <a href="https://github.com/darienpayton/darienpayton-website">
-                Code
-              </a>
-            </div>
           </div>
         </div>
       </div>
