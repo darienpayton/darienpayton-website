1199d87eaeddf5b38049a0863630d1b445aaae8b
Darien Payton <darien.payton71@gmail.com>
2019-10-28 09:21:00 -0500

    added archives redirect and styling

--- src/pages/newsletter/Newsletter.js
+++ src/pages/newsletter/Newsletter.js
@@ -3,11 +3,24 @@ import Background from '../../components/Background';
 import NavBar from '../../components/NavBar';
 import Subscribe from '../../components/Subscribe';
 
+const styles = {
+  color: '#226597',
+  marginTop: '20px',
+  textAlign: 'center'
+};
+
+const aStyles = {
+  textDecoration: 'none'
+};
+
 function Newsletter() {
   return (
     <Background>
       <NavBar />
       <Subscribe />
+      <a href="/newsletter/archives" style={aStyles}>
+        <p style={styles}>Show me past letters &#8594;</p>
+      </a>
     </Background>
   );
 }
